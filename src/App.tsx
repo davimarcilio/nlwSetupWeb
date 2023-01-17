eb2efaae480a63d38daa699b744bfc1a6528b685
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-17 20:39:02 -0300

    Header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Habit } from "./components/Habit";
+// import { Habit } from "./components/Habit";
+import { Header } from "./components/Header";
 
 export function App() {
   return (
-    <div>
-      <Habit completed={2} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
+      {/* <Habit completed={2} /> */}
     </div>
   );
 }
