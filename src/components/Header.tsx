ba14c3b6f4a59ea2aa731b0ac6ef72021b1395a3
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-18 20:03:05 -0300

    new habit form modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,34 @@
-import { Plus } from "phosphor-react";
+import { Plus, X } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
 import Logo from "../assets/Logo.svg";
+import { NewHabitForm } from "./NewHabitForm";
 
 export function Header() {
   return (
     <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
       <img src={Logo} alt="Habits" />
-      <button
-        className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center justify-center gap-3 transition-colors hover:border-violet-300"
-        type="button"
-      >
-        <Plus size={20} className={"text-violet-500"} />
-        Novo hábito
-      </button>
+      <Dialog.Root>
+        <Dialog.Trigger
+          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center justify-center gap-3 transition-colors hover:border-violet-300"
+          type="button"
+        >
+          <Plus size={20} className={"text-violet-500"} />
+          Novo hábito
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="w-screen h-screen bg-black opacity-80 fixed inset-0" />
+
+          <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 -translate-y-1/2  left-1/2 -translate-x-1/2">
+            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200 transition-colors">
+              <X size={24} aria-label="Fechar" />
+            </Dialog.Close>
+            <Dialog.Title className="text-3xl leading-tight font-extrabold">
+              Criar hábito
+            </Dialog.Title>
+            <NewHabitForm />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
