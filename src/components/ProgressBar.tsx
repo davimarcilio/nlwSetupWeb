4b498f92aa8a6b8162e3f0cbe4c1572ef80c226e
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-18 20:15:33 -0300

    ProgressBar

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,20 @@
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar({ progress }: ProgressBarProps) {
+  const progressStyles = {
+    width: `${progress}%`,
+  };
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role={"progressbar"}
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={progress}
+        className="h-3 rounded-xl bg-violet-600"
+        style={progressStyles}
+      ></div>
+    </div>
+  );
+}
